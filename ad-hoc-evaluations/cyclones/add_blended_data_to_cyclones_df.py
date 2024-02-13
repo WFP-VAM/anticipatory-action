91f1d79604aa4653553e275482369d782eeb37ad
AmineBarkaoui <66622330+AmineBarkaoui@users.noreply.github.com>
2024-02-13 15:07:50 +0100

    merge blended data with cyclones df

---
+++ ad-hoc-evaluations/cyclones/add_blended_data_to_cyclones_df.py
@@ -0,0 +1,64 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# Run this notebook using the HDC environment.
+
+# ### Imports
+
+# +
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+import warnings
+warnings.simplefilter('ignore')
+# -
+
+# ### Read cyclones data
+
+cyclones = pd.read_csv('MOZ_overland_ibtracs.since1980.list.v04r00.txt')
+
+cyclones
+
+# ### Read blended data 
+
+blended = xr.open_zarr('/s3/scratch/public-share/long_term_blending/MOZ/chirp_daily_blended_corrected_dek.zarr')
+
+# Mask no data
+blended = blended.where(blended != blended.daily_blended.nodata, np.nan)
+
+blended
+
+# ### Add blended data into cyclones dataframe 
+
+# +
+for i, row in cyclones.iterrows():
+    
+    t = pd.to_datetime(row.ISO_TIME[:10])
+    lon = row.LON
+    lat = row.LAT
+    
+    try: 
+        cyclones.loc[i, 'BLENDED_CHIRP'] = blended.sel(time=t).sel(longitude=lon, latitude=lat, method='nearest').daily_blended.values
+    except:
+        cyclones.loc[i, 'BLENDED_CHIRP'] = np.nan 
+# -
+
+cyclones
+
+# ### Save updated dataframe 
+
+cyclones.to_csv('MOZ_overland_ibtracs.since1980.list.v04r00.blended.txt')
+
