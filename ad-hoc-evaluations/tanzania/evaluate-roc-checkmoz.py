4d094365d10a22bede5533739fc9cf97e6509bb7
AmineBarkaoui <amine.barkaoui1@gmail.com>
2024-04-18 09:28:18 +0000

    add tanzania verification nbs

---
+++ ad-hoc-evaluations/tanzania/evaluate-roc-checkmoz.py
@@ -0,0 +1,199 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: hdc
+#     language: python
+#     name: conda-env-hdc-py
+# ---
+
+# This notebook aims to evaluate the forecast skill (ROC score) for the JFM period using forecasts issued in January for Mozambique. It uses HDC datasets so we can compare the ROC scores with the ones obtained using local forecasts and observations datasets. 
+
+# +
+# %cd ../../
+
+import dask
+import logging
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from dask.distributed import Client
+
+from config.params import Params
+
+from helper_fns import aggregate_by_district
+from analytical import calculate_forecast_probabilities
+
+from hip.analysis.aoi.analysis_area import AnalysisArea
+from hip.analysis.ops._statistics import evaluate_roc_forecasts
+
+# +
+issue = "11"
+
+period_months = (1, 2, 3) # JFM
+
+params = Params(iso='MOZ', index='SPI')
+# -
+
+DATES = "1981-01-01/2023-12-31"
+area = AnalysisArea.from_admin_boundaries(iso3='MOZ', admin_level=2, resolution=0.25, datetime_range=DATES)
+area.geobox
+
+gdf = area.get_dataset([area.BASE_AREA_DATASET])
+gdf.columns = ['geometry', 'code', 'ADM2_PT']
+
+forecasts = area.get_dataset(['ECMWF', f'RFH_FORECASTS_SEAS5_ISSUE{int(issue)}_DAILY'],                      
+    load_config={
+        "gridded_load_kwargs": {
+             "resampling": "bilinear",
+        }
+    }
+)
+forecasts = forecasts.where(
+    forecasts.time < np.datetime64(f"{params.year}-07-01T12:00:00.000000000"),
+    drop=True,
+).to_dataset()
+forecasts.attrs['nodata'] = np.nan
+forecasts.tp.attrs['nodata'] = np.nan
+
+forecasts = forecasts.isel(time=slice(2547, None)).load()
+
+observations = area.get_dataset(['CHIRPS', 'RFH_DAILY'],                      
+    load_config={
+        "gridded_load_kwargs": {
+             "resampling": "bilinear",
+        }
+    }
+).rename('precip').to_dataset()
+observations.attrs['nodata'] = observations.precip.nodata
+
+observations.load()
+
+forecasts.isel(longitude=0, latitude=0, ensemble=0).groupby('time.year').mean().tp.plot.line() # , time=slice(3000,3100)
+
+# +
+import datetime
+from hip.analysis.analyses.drought import run_accumulation_index
+
+# Remove 1980 season to harmonize observations between different indexes
+if int(issue) >= params.end_season:
+    observations = observations.where(
+        observations.time.dt.date >= datetime.date(1981, 10, 1), drop=True
+    )
+
+# Accumulation
+accumulation_fc = run_accumulation_index(
+    forecasts, params.aggregate, period_months, forecasts=True
+)
+accumulation_obs = run_accumulation_index(
+    observations, params.aggregate, period_months
+)
+logging.info(f"Completed accumulation")
+# -
+
+accumulation_fc.isel(longitude=0, latitude=0, ensemble=0).tp.plot.line()
+
+# +
+from hip.analysis.analyses.drought import run_gamma_standardization
+
+# Remove potential inconsistent observations
+accumulation_obs = accumulation_obs.sel(
+    time=slice(datetime.date(1979, 1, 1), datetime.date(params.year - 1, 12, 31))
+)
+
+# TODO handle attributes in accumulation
+accumulation_fc.tp.attrs = forecasts.tp.attrs
+accumulation_obs.precip.attrs = observations.precip.attrs
+
+# Anomaly
+anomaly_fc = run_gamma_standardization(
+    accumulation_fc, params.calibration_start, params.calibration_stop, members=True,
+)
+anomaly_obs = run_gamma_standardization(
+    accumulation_obs,
+    params.calibration_start,
+    params.calibration_stop,
+)
+logging.info(f"Completed anomaly")
+# -
+
+anomaly_fc.where(anomaly_fc.time.dt.day == 1, drop=True).isel(longitude=0, latitude=0).tp.plot.line()
+
+(anomaly_fc.where(anomaly_fc.time.dt.day == 1, drop=True).isel(longitude=0, latitude=0) < -1000).tp.plot.line()
+
+probs, probs_bc, obs_values, obs_bool = calculate_forecast_probabilities(
+    forecasts,
+    observations,
+    params,
+    period_months,
+    issue,
+)
+
+probs.isel(longitude=0, latitude=0, category=0).tp.plot.line()
+
+# +
+import xskillscore as xss
+
+def evaluate_roc_forecasts(observations, *forecasts, dim="year"):
+    """
+    Calculate the area under the ROC curve scores of forecasts dataset(s)
+
+    Args:
+        observations: xarray.DataArray, categorical observations at the pixel level for specific index
+        *forecasts: xarray.DataArray object(s) containing forecasts as probabilities to evaluate for specific index and issue month
+        dim: str or list, dimension(s) over which to compute the contingency table
+    Returns:
+        auc: tuple of the same length as *forecasts of xarray.DataArray, reduced by dimensions dim, containing area under ROC curve
+    """
+
+    aucs = []
+
+    # Compute AUC for each forecasts xarray object
+    for fc in forecasts:
+        auc = xss.roc(observations.sel(year=fc.year), fc, dim=dim, return_results="area")
+        # set AUC as NaN where no rain in either chirps or forecasts (replicate R method)
+        auc = auc.where((observations.sum(dim) != 0) & (fc.sum(dim) != 0), np.nan)
+        aucs.append(auc)
+
+    return tuple(aucs)
+
+
+# -
+
+auc, auc_bc = evaluate_roc_forecasts(
+    obs_bool.precip,
+    probs.tp,
+    probs_bc.scen,
+)
+
+auc.isel(longitude=0, latitude=0, category=0)
+
+auc.to_zarr("ad-hoc-evaluations/tanzania/outputs/moz/moz_auc_jfm_issue11.zarr")
+
+auc_bc.to_zarr("ad-hoc-evaluations/tanzania/outputs/moz/moz_auc_bc_jfm_issue11.zarr")
+
+# Aggregate by district
+auc_district = aggregate_by_district(auc, gdf, params)
+auc_bc_district = aggregate_by_district(auc_bc, gdf, params)
+
+# Read ref roc scores
+moz_ref = pd.read_csv("ad-hoc-evaluations/tanzania/outputs/moz/fbf.districts.roc.spi.2022.txt")
+
+# Compare
+moz_ref = moz_ref.loc[moz_ref.district.isin(auc_district.district.values)]
+moz_ref = moz_ref.loc[(moz_ref.Index == 'SPI JFM') & (moz_ref.issue == 11)]
+auc_hdc = np.array([auc_bc_district.sel(district=r.district, category=r.category).values if r.BC else auc_district.sel(district=r.district, category=r.category).values for _, r in moz_ref.iterrows()]).astype(float)
+moz_ref['auc_hdc'] = auc_hdc
+moz_ref['diff (hdc - ref)'] = moz_ref.auc_hdc - moz_ref.AUC_best
+
+moz_ref
+
+moz_ref['diff (hdc - ref)'].hist(bins=15)
+
+
---
+++ ad-hoc-evaluations/tanzania/evaluate-roc-tanzania.py
@@ -0,0 +1,187 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: hdc
+#     language: python
+#     name: conda-env-hdc-py
+# ---
+
+# This notebook aims to calculate the forecast skill (ROC score) over Tanzania for the OND, JFM and MAM periods. It uses HDC datasets (read through hip-analysis). This notebook can be run using the HDC environment. 
+
+# +
+# %cd ../../
+
+import dask
+import logging
+import numpy as np
+import xarray as xr
+
+from dask.distributed import Client
+
+from config.params import Params
+
+from analytical import calculate_forecast_probabilities
+
+from hip.analysis.aoi.analysis_area import AnalysisArea
+from hip.analysis.ops._statistics import evaluate_roc_forecasts
+
+# +
+issue = "1"
+
+params = Params(iso='TZA', index='SPI')
+# -
+
+DATES = "1981-01-01/2023-12-31"
+area = AnalysisArea.from_admin_boundaries(iso3='TZA', admin_level=2, resolution=0.25, datetime_range=DATES)
+area.geobox
+
+gdf = area.get_dataset([area.BASE_AREA_DATASET])
+
+forecasts = area.get_dataset(['ECMWF', f'RFH_FORECASTS_SEAS5_ISSUE{int(issue)}_DAILY'],                      
+    load_config={
+        "gridded_load_kwargs": {
+             "resampling": "bilinear",
+        }
+    }
+)
+forecasts = forecasts.where(
+    forecasts.time < np.datetime64(f"{params.year}-07-01T12:00:00.000000000"),
+    drop=True,
+).to_dataset()
+forecasts.attrs['nodata'] = np.nan
+forecasts.tp.attrs['nodata'] = np.nan
+
+# + jupyter={"outputs_hidden": true}
+forecasts.load()
+# -
+
+observations = area.get_dataset(['CHIRPS', 'RFH_DAILY'],                      
+    load_config={
+        "gridded_load_kwargs": {
+             "resampling": "bilinear",
+        }
+    }
+).rename('precip').to_dataset()
+observations.attrs['nodata'] = observations.precip.nodata
+
+area_lta = AnalysisArea.from_admin_boundaries(iso3='TZA', admin_level=2, resolution=0.25, datetime_range="1970-01-01/1970-12-31")
+obs_lta = area_lta.get_dataset(['CHIRPS', 'R3H_DEKAD_LTA'],                      
+    load_config={
+        "gridded_load_kwargs": {
+             "resampling": "bilinear",
+        }
+    }
+)
+
+observations.load()
+
+# +
+period_months = (3, 4, 5) # MAM
+
+probs, probs_bc, obs_values, obs_bool = calculate_forecast_probabilities(
+    forecasts,
+    observations,
+    params,
+    period_months,
+    issue,
+)
+
+# +
+import xskillscore as xss
+
+def evaluate_roc_forecasts(observations, *forecasts, dim="year"):
+    """
+    Calculate the area under the ROC curve scores of forecasts dataset(s)
+
+    Args:
+        observations: xarray.DataArray, categorical observations at the pixel level for specific index
+        *forecasts: xarray.DataArray object(s) containing forecasts as probabilities to evaluate for specific index and issue month
+        dim: str or list, dimension(s) over which to compute the contingency table
+    Returns:
+        auc: tuple of the same length as *forecasts of xarray.DataArray, reduced by dimensions dim, containing area under ROC curve
+    """
+
+    aucs = []
+
+    # Compute AUC for each forecasts xarray object
+    for fc in forecasts:
+        auc = xss.roc(observations.sel(year=fc.year), fc, dim=dim, return_results="area")
+        # set AUC as NaN where no rain in either chirps or forecasts (replicate R method)
+        auc = auc.where((observations.sum(dim) != 0) & (fc.sum(dim) != 0), np.nan)
+        aucs.append(auc)
+
+    return tuple(aucs)
+
+
+# -
+
+auc, auc_bc = evaluate_roc_forecasts(
+    obs_bool.precip,
+    probs.tp,
+    probs_bc.scen,
+)
+
+auc.to_zarr("ad-hoc-evaluations/tanzania/outputs/SPI MAM/auc_mam_issue1.zarr")
+
+auc_bc.to_zarr("ad-hoc-evaluations/tanzania/outputs/SPI MAM/auc_bc_mam_issue1.zarr")
+
+# ####  Visualization
+
+period = 'OND'
+iss = 8
+
+auc = xr.open_zarr(f"ad-hoc-evaluations/tanzania/outputs/SPI {period}/auc_{period.lower()}_issue{iss}.zarr").histogram_observations_forecasts
+auc_bc = xr.open_zarr(f"ad-hoc-evaluations/tanzania/outputs/SPI {period}/auc_bc_{period.lower()}_issue{iss}.zarr").histogram_observations_forecasts
+
+# +
+import os
+from odc.geo.xr import write_cog
+
+for c in auc.category:
+    if not os.path.exists(f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI {period}/{c.values}"):
+        os.mkdir(f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI {period}/{c.values}")
+        
+    write_cog(auc.sel(category=c.values), f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI {period}/{c.values}/auc_{str(c.values).lower()}_{period.lower()}_issue{iss}.tif", overwrite=True)
+    write_cog(auc_bc.sel(category=c), f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI {period}/{c.values}/auc_bc_{str(c.values).lower()}_{period.lower()}_issue{iss}.tif", overwrite=True)
+    
+    figure = auc.sel(category=c.values).hip.viz.map(title=f'AUC {c.values} {period}', cmap='RdYlGn')
+    figure.savefig(f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI {period}/{c.values}/auc_{str(c.values).lower()}_{period.lower()}_issue{iss}.png")
+    
+    figure = auc_bc.sel(category=c.values).hip.viz.map(title=f'AUC BC {c.values} {period}', cmap='RdYlGn')
+    figure.savefig(f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI {period}/{c.values}/auc_bc_{str(c.values).lower()}_{period.lower()}_issue{iss}.png")
+# -
+
+# Without bias correction
+cat = 'Moderate'
+auc.sel(category=cat).hip.viz.map(title=f'AUC {cat} {period}', cmap='RdYlGn') #.plot.imshow()
+
+# With bias correction
+auc_bc.sel(category=cat).hip.viz.map(title=f'AUC BC {cat} {period}', cmap='RdYlGn')
+
+# LTA
+
+import datetime
+obs_ond = obs_lta.sel(time=datetime.datetime(1970, 12, 1))
+obs_jfm = obs_lta.sel(time=datetime.datetime(1970, 3, 1))
+obs_mam = obs_lta.sel(time=datetime.datetime(1970, 5, 1))
+
+from hdc.colors.rainfall import rxs
+figure = obs_ond.drop_vars('time').hip.viz.map(title='RFH LTA OND', cmap=rxs.cmap)
+figure.savefig(f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI OND/rfh_lta_ond.png")
+
+figure = obs_jfm.drop_vars('time').hip.viz.map(title='RFH LTA JFM', cmap=rxs.cmap)
+figure.savefig(f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI JFM/rfh_lta_jfm.png")
+
+figure
+
+figure = obs_mam.drop_vars('time').hip.viz.map(title='RFH LTA MAM', cmap=rxs.cmap)
+figure.savefig(f"ad-hoc-evaluations/tanzania/outputs/rasters/SPI MAM/rfh_lta_mam.png")
+figure
+
+
