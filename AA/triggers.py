74f4170d33cfcaf70f7833e47daeb53ff06482e4
AmineBarkaoui <66622330+AmineBarkaoui@users.noreply.github.com>
2024-10-18 10:40:28 +0200

    fix path splitting in triggers to avoid machine specificities

--- AA/triggers.py
+++ AA/triggers.py
@@ -2,7 +2,7 @@ import logging
 
 import click
 
-logging.basicConfig(level="INFO")
+logging.basicConfig(level="INFO", force=True)
 
 import warnings
 
@@ -523,7 +523,7 @@ def read_aggregated_obs(path_to_zarr, params):
 
     obs = xr.Dataset({"bool": obs_bool, "val": obs_val})
     obs["time"] = [pd.to_datetime(t).year for t in obs.time.values]
-    obs["index"] = [i.split("/")[-2] for i in list_val_paths]
+    obs["index"] = [os.path.split(os.path.dirname(i))[-1] for i in list_val_paths]
     return obs
 
 
@@ -539,7 +539,7 @@ def read_aggregated_probs(path_to_zarr, params):
             os.path.join(i, "probabilities_bc.zarr")
             for i in glob.glob(f"{l}/{params.index} *")
         ]
-        index_names = [i.split("/")[-2] for i in list_index_raw]
+        index_names = [os.path.split(os.path.dirname(i))[-1] for i in list_index_raw]
 
         try:
             index_raw = xr.open_mfdataset(
@@ -559,7 +559,7 @@ def read_aggregated_probs(path_to_zarr, params):
 
             ds_index = xr.Dataset({"raw": index_raw, "bc": index_bc})
             ds_index["index"] = index_names
-            list_index[int(l.split("/")[-1])] = ds_index
+            list_index[int(os.path.split(l)[-1])] = ds_index
         except:
             continue
 
