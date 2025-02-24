b7b181d245cc8bd34a8f110958d378a975701a1f
AmineBarkaoui <amine.barkaoui1@gmail.com>
2025-02-24 16:53:00 +0000

    switch to zonal stats in distrct aggregation helper function

--- AA/helper_fns.py
+++ AA/helper_fns.py
@@ -50,27 +50,63 @@ def triggers_da_to_df(triggers_da, hr_da):
     return triggers_df.reset_index().drop_duplicates()
 
 
-def aggregate_by_district(ds, gdf, params):
-    PROJ = "+proj=longlat +ellps=clrk66 +towgs84=-80,-100,-228,0,0,0,0 +no_defs"
+def compute_district_average(da, area):
+    """
+    Computes zonal statistics on an xarray DataArray for both observations and probabilities.
 
-    # Clip ds to districts
-    list_districts = {}
-    for _, row in gdf.iterrows():
-        try:
-            list_districts[row["Name"]] = (
-                ds.rio.write_crs(PROJ)
-                .rio.clip(gpd.GeoSeries(row.geometry))
-                .mean(dim=["latitude", "longitude"])
-            )
-        except:
-            continue
+    Args:
+        da : xarray.DataArray, Input DataArray (can be observations or probabilities).
+        area : hip.analysis.aoi.analysis_area.AnalysisArea: object characterizing the area 
+            and admin level of interest.
+    Returns: xarray.DataArray, DataArray with computed district averages.
+    """
+    # Temporary (TODO fix hip-analysis.zonal_stats): filter zone_ids based on exclusions
+    DISTRICTS_TO_EXCLUDE = [
+        "Cidade_De_Pemba",
+        "Ibo",
+        "Pemba",
+        "Cidade_De_Xai-Xai",
+        "Cidade_De_Inhambane",
+        "Maxixe",
+        "Cidade_De_Chimoio",
+        "Cidade_Da_Matola",
+        "Cidade_De_Nampula",
+        "Ilha_De_Moçambique",
+        "Cidade_De_Lichinga",
+        "Maquival",
+    ]
+    gdf = area.get_dataset([area.BASE_AREA_DATASET])
+    zone_ids = gdf.loc[~gdf.Name.isin(DISTRICTS_TO_EXCLUDE)].Name.values
+    
+    # Ensure consistent time dimension
+    if "year" in da.dims:
+        da = da.rename({'year': 'time'})
+    
+    # Determine dimensions to group by (exclude spatial dimensions)
+    groupby_dim = set(da.dims) - {"latitude", "longitude", "time"}
 
-    ds_by_district = xr.concat(
-        list_districts.values(), pd.Index(list_districts.keys(), name="district")
-    )
-    ds_by_district["district"] = ds_by_district.district.astype(str)
+    # Transpose dims to ensure equality of shapes
+    da = da.transpose(..., *groupby_dim, 'latitude', 'longitude')
 
-    return ds_by_district
+    # Compute zonal stats: handle different groupby dimensions lengths
+    if len(groupby_dim) > 1:
+        raise NotImplementedError("Zonal stats with more than one groupby dimension are not supported.")
+    elif len(groupby_dim) == 1:
+        da_grouped = da.groupby(*groupby_dim).map(
+            lambda da: area.zonal_stats(
+                da.squeeze(groupby_dim), stats=['mean'], zone_ids=zone_ids, zones=None
+            ).to_xarray()['mean']
+        )
+    else:
+        da_grouped = area.zonal_stats(da, stats=['mean'], zone_ids=zone_ids, zones=None).to_xarray()['mean']
+        
+    # Rename 'zone' to 'district' for consistency
+    da_grouped = da_grouped.rename({"zone": "district"})
+
+    # Ensure district is a string type
+    da_grouped["district"] = da_grouped.district.astype(str)
+
+    return da_grouped
 
 
 def merge_un_biased_probs(probs_district, probs_bc_district, params, period_name):
@@ -192,7 +228,7 @@ def load_trigger_with_reference(params, variant_folder=None):
 
 def merge_probabilities_triggers_dashboard(probs, triggers, params, period):
     # Format probabilities
-    probs_df = probs.to_dataframe().reset_index().drop("spatial_ref", axis=1)
+    probs_df = probs.to_dataframe().reset_index()
     probs_df["prob"] = [np.round(p, 2) for p in probs_df.prob.values]
     probs_df["index"] = probs_df["index"].str.upper()
     probs_df["aggregation"] = np.repeat(
